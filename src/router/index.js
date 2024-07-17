05124a6166642faa84021f56b4b2cd7f37ffa1bd
hedong <2732655542@qq.com>
2024-07-17 16:32:26 +0800

    清空gh-pages分支代码

--- src/router/index.js
+++
@@ -1,8 +0,0 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import routes from './router.config.js'
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-})
-
-export default router
